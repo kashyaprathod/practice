a360b4b4eb4cb65db77392e509a805aa527c1d00
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-14 23:03:03 +0530

    pattern5 java file

---
+++ pattern5.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class pattern5 {
+	public static void main(String args[]){
+		for(int i=1;i<=5;i++){
+			for(int j=5;j>=i;j--){
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
