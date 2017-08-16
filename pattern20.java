47c30fd3a99a61e30b9a4ff153523f2eacbbcc92
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-16 22:55:23 +0530

    pattern20 java file

---
+++ pattern20.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class pattern20 {
+	public static void main(String args[]){
+		for(int i=1;i<=5;i++){
+			for(int j=1;j<=5;j++){
+				if(i==j && j==j){
+					System.out.print("$");
+				}else{
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}	
+}
