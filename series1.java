4f9343aef34b3e4b5bb426b5a0069976571b8b2b
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-17 22:22:36 +0530

    series1 java file

---
+++ series1.java
@@ -0,0 +1,12 @@
+package series;
+
+public class series1 {
+	public static void main(String args[]){
+		System.out.println("printing numbers from 1-10");
+		
+		for(int i=1;i<=10;i++){
+			System.out.println(i);
+		}
+	}
+	
+}
