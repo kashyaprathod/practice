2c1d1bf589b07daa84df01f9ed5d35ecceb7da63
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-18 22:55:23 +0530

    series11 java file

---
+++ series11.java
@@ -0,0 +1,16 @@
+package series;
+import java.util.*;
+public class series11 {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("enter n: ");
+		int n= sc.nextInt();
+		double sum =0;
+		for(int i=1;i<=n;i++){
+			sum += (1.0/i);
+		}
+		System.out.println(sum);
+	}
+	
+}
