bd903c2c8b27803505cc4d8faa3108eade13811a
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-19 22:50:03 +0530

    factorial java file

---
+++ factorial.java
@@ -0,0 +1,17 @@
+package series;
+import java.util.*;
+public class factorial {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("factorial");
+		
+		System.out.println("enter n: ");
+		int n = sc.nextInt();
+		int fact =1;
+		for(int i =2;i<=n;i++){
+			fact *=i;
+		}
+		System.out.println(fact);
+	}
+	
+}
