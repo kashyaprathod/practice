cfcbc8cd2da950566e5f67d245288ec0688b20f2
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-21 22:29:20 +0530

    numbers14 java file

---
+++ numbers14.java
@@ -0,0 +1,23 @@
+public class numbers14 {
+	public static void main(String args[]) {
+		String primeNo = "";
+		int j = 0;
+		int LastPrime = 1;
+		System.out.println("Twin Primes are:");
+		for (int i = 1; i < 200; i++) {
+			for (j = 2; j <=i; j++) {
+			   if (i % j == 0) {
+			   break;
+			   }
+			}
+			if (i == j) {
+			   primeNo += i + " ";
+			   if ((i - LastPrime) == 2) {
+			   System.out.println("("+(i-2)+","+i+")");
+			   }
+			   LastPrime = i;
+			}
+		}
+		System.out.println("Prime Numbers are: " + primeNo);
+	}
+}
