eb48bcaa0eda202d9bcd35f0aa6d86e035645556
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-25 23:05:46 +0530

    ContinueUse java file

---
+++ ContinueUse.java
@@ -0,0 +1,10 @@
+public class ContinueUse{
+    public static void main(){
+        int a;
+        for(a=0;a<=20;a++){
+            if(a==9)
+                continue;
+            System.out.println(a + " ");
+        }
+    }
+}
