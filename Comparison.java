099048782851283374f5407105a93aef4263cf28
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-25 23:04:23 +0530

    Comparison java file

---
+++ Comparison.java
@@ -0,0 +1,10 @@
+public class Comparison{
+    void greater(int a, int b){
+        int c;
+        if(a>b)
+            c=a;
+        else
+            c=b;
+        System.out.println("The greater number is "+c);
+    }
+}
