026a072a0839a9b739fc8ad2c11b423ec0059a28
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-26 23:01:00 +0530

    DayName1 java file

---
+++ DayName1.java
@@ -0,0 +1,20 @@
+public class DayName1{
+    void printEquivalentDayName(int day_no){
+        if(day_no==1)
+            System.out.println("Monday");
+        else if(day_no==2)
+            System.out.println("Tuesday");
+        else if(day_no==3)
+            System.out.println("Wednesday");
+        else if(day_no==4)
+            System.out.println("Thursday");
+        else if(day_no==5)
+            System.out.println("Friday");
+        else if(day_no==6)
+            System.out.println("Saturday");
+        else if(day_no==7)
+            System.out.println("Sunday");
+        else
+            System.out.println("Invalid day number");
+    }
+}
