8e2bb7d188acd991669bf99ea42db1436e981b51
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-26 23:03:26 +0530

    EvenOdd java file

---
+++ EvenOdd.java
@@ -0,0 +1,8 @@
+public class EvenOdd{
+    void checkEvenOdd(int num){
+        if(num%2==0)
+            System.out.println("Number is even");
+        else
+            System.out.println("Number is odd");
+    }
+}
