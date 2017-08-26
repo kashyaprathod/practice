940a51023cdd8ee78f1d9ff1a365abd75f025e41
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-26 23:03:04 +0530

    EquivalentDayName java file

---
+++ EquivalentDayName.java
@@ -0,0 +1,29 @@
+public class EquivalentDayName{
+    public static void printEquivalentDayName(int day_no){
+        switch(day_no){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Ivalid day number");
+        }//end of switch 
+    }//end of function 
+}//end of class 
