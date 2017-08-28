b2fc159febb0642242d50cc096512eca8826aa25
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-28 22:56:08 +0530

    hmm java file

---
+++ hmm.java
@@ -0,0 +1,13 @@
+public class hmm{
+    public static void main(){
+        char var = 'a';
+        switch(var){
+            case 'a':
+            System.out.println("A");
+            case 'b':
+            System.out.println("B");
+            default:
+            System.out.println("c");
+        }
+    }
+}
