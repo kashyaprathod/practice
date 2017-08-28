27341190999de7ea673c1465fd17d4b57617e532
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-28 22:55:15 +0530

    Vowel java file

---
+++ Vowel.java
@@ -0,0 +1,9 @@
+public class Vowel{
+    public static void main(char ch){
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+            System.out.println("it is a vowel");
+        }else{
+            System.out.println("it is a consonant");
+        }
+    }
+}
