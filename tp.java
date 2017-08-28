c206a39bf3ff405b47bf277f2d3657c51f956473
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-28 22:56:36 +0530

    tp java file

---
+++ tp.java
@@ -0,0 +1,12 @@
+public class tp{
+    public static void main(){
+        int a = 50;
+        while(true)
+        {
+            if(a<10)
+            break;
+            a = a-10;
+        }
+        System.out.println("a is "+a);
+    }
+}
