64ef5b4ce25a6078c2d51ef7f78c8cde1c9c0adf
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-28 22:54:29 +0530

    Table java file

---
+++ Table.java
@@ -0,0 +1,9 @@
+public class Table{
+    public static void FindTable(int n){
+        int a;
+        System.out.println("========================");
+        for(a=1;a<=10;a++){
+            System.out.println(n + "X" + a + " = "+n*a);
+        }
+    }
+}
