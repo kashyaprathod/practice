32ea5916c159e32a78584a4d335953243d2bdd04
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-30 23:49:07 +0530

    Chapter3 java file

---
+++ Chapter3.java
@@ -0,0 +1,14 @@
+public class Chapter3{
+    public static void matnFuncton(){
+        System.out.println("MAth.abs(-73) = "+Math.abs(-73));
+        System.out.println("Math.sqrt(49) = "+(int)Math.sqrt(49));
+        System.out.println("Math.pow(3,2) = "+Math.pow(3,2));
+        System.out.println("Math.round(1.5) "+Math.round(1.5));
+        System.out.println("Math.ceil(15.3) = "+Math.ceil(15.3));
+        System.out.println("Math.floor(15.3) = "+Math.floor(15.3));
+        System.out.println("Math.max(100, 2) = "+Math.max(100, 2));
+        System.out.println("Math.min(49, 53) = "+Math.max(100, 2));
+        System.out.println((int)Math.random()*100);
+        
+    }
+}
