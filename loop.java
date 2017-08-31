73983efeb7458b8f835391279149740c6e2fa347
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-31 21:31:50 +0530

    loop java file

---
+++ loop.java
@@ -0,0 +1,19 @@
+import java.lang;
+import java.util.Scanner;
+public class loop{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows");
+        int rows = sc.nextInt();
+        System.out.println("enter number of colums");
+        int colum = sc.nextInt();
+        for(int i=1;i<=rows;i++){
+            
+            for(int j=1;j<=colum;j++){
+                System.out.print("@ ");
+                }
+            }
+           System.out.println();
+        }
+       
+    }
