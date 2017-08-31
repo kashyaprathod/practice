01bc9c4b3c743c19cbf8ab33f7600bee1ec87e8b
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-08-31 21:30:36 +0530

    Missingbreak java file

--- Missingbreak.java
+++ Missingbreak.java
@@ -1,26 +1,22 @@
-public class Missingbreak{
-    public static void Test(int month_no){
-        switch(month_no){
+public class missingbreak{
+    public static void daysofmonth(int month){
+        switch(month){
             case 1:
             case 3:
             case 5:
             case 7:
-            case 8:
-            case 10:
-            case 12:
-                System.out.println ("This month has 31 days");
-                break;
-            case 4:
-            case 6:
             case 9:
             case 11:
-                System.out.prinln("This month has 30 days");
-                break;
+            case 12:
+            System.out.println("this month has 31 months");
             case 2:
-                System.out.println("This month has 28 days");
-                break;
+            System.out.println("this month has 28 days");
+            case 6:
+            case 8:
+            case 10:
+            System.0ut.println("this month has 30 days");
             default:
-                System.out.println("Invalid month number");
-        }//end of switch 
-    }//end of function
-}//end of class //
+            System.out.println("invalid month");
+        }
+    }
+}
