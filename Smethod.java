c89220908613ac40f3eab90ea330223733b54b5c
Kashyap Rathod <30984021+kashyaprathod@users.noreply.github.com>
2017-09-05 21:51:32 +0530

    Smethod java file

---
+++ Smethod.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+public class Smethod{
+    int num1 = 50;
+    static int num2 = 100;
+    public void call2(){
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+    public static void call1(){
+        //System.out.println(num1);
+        System.out.println(num2);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
